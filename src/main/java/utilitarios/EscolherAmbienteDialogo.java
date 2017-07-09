dc461676bdb1751593c24630997e97d9722feebd
unknown <pedrorangel-10@hotmail.com>
2017-07-09 11:11:58 -0300

    refatorando dialogo

--- src/main/java/utilitarios/EscolherAmbienteDialogo.java
+++ src/main/java/utilitarios/EscolherAmbienteDialogo.java
@@ -1,16 +1,14 @@
 package utilitarios;
 
-import javafx.scene.control.ChoiceDialog;
-
 import java.util.List;
 import java.util.Optional;
 
+import javafx.scene.control.ChoiceDialog;
+
 public class EscolherAmbienteDialogo {
 
-    private static ChoiceDialog<String> dialogo;
-
     public static String nomeDialogo(String titulo, String cabecalho, String conteudo, List<String> opcoes) {
-        dialogo = new ChoiceDialog<>(null, opcoes);
+        ChoiceDialog<String> dialogo = new ChoiceDialog<>(null, opcoes);
         dialogo.setTitle(titulo);
         dialogo.setHeaderText(cabecalho);
         dialogo.setContentText(conteudo);
